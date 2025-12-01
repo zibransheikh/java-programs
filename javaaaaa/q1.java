2854c0c5b37e079ec55cba8b3a8f555b22b44e80
zibransheikh <zibran.shaikh99@gmail.com>
2025-12-01 17:47:39 +0530

    Update and rename chetan.java to q1.java

--- javaaaaa/q1.java
+++ javaaaaa/q1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class chetan {
+public class q1 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -16,3 +16,4 @@ public class chetan {
     
     }
 }
+

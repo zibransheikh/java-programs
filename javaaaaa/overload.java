9bed883b27bbe63abe0d9ae2bfc92404d446159e
zibransheikh <zibran.shaikh99@gmail.com>
2025-12-01 17:50:02 +0530

    Update overload.java

--- javaaaaa/overload.java
+++ javaaaaa/overload.java
@@ -1,26 +1,23 @@
 class Rectangle {
     int length, width;
 
-    // Constructor with two parameters
     Rectangle(int l, int w) {
         length = l;
         width = w;
     }
 
-    // Constructor with one parameter (square)
     Rectangle(int side) {
         length = width = side;
     }
 
-    // Method
     int area() {
         return length * width;
     }
 }
 public class overload {
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(10, 5); // rectangle
-        Rectangle r2 = new Rectangle(7);     // square
+        Rectangle r1 = new Rectangle(10, 5); 
+        Rectangle r2 = new Rectangle(7);     
 
         System.out.println("Area of Rectangle: " + r1.area());
         System.out.println("Area of Square: " + r2.area());
